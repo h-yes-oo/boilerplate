78654f3b778392be5458a3021d99b1d8f8efe9c5
h-yes-oo <hyesoo5115@naver.com>
2020-08-31 16:39:10 +0900

    auth

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
-
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -17,7 +17,7 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB connected...')).catch(err => console.log(err))
 
 app.get('/',(req,res) => res.send('hellooooo World!'))
-app.post('/register',(req,res)=>{
+app.post('/api/users/register',(req,res)=>{
     const user = new User(req.body);
     user.save((err,userInfo) => {
         if(err) return res.json({ success: false, err})
@@ -27,7 +27,7 @@ app.post('/register',(req,res)=>{
     })
 })
 
-app.post('/login',(req,res) => {
+app.post('/api/users/login',(req,res) => {
     //요청된 이메일이 데이터베이스에 있는지 확인
     User.findOne({ email: req.body.email }, (err, user) => {
         if(!user) {
@@ -52,6 +52,20 @@ app.post('/login',(req,res) => {
     })
 })
 
-app.listen(port,()=>console.log(`Example app listening on port ${port}!`))
+app.get('/api/users/auth', auth , (req,res) => {
+    //여기까지 미들웨어를 통과해 왔다는 것은, Authentication이 True라는 의미
+    res.status(200).json({
+        _id: req.user._id,
+        //role이 0이면 일반 유저이고, 0이 아니면 관리자라고 규칙을 정했을 때
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        emil: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
 
-// mongodb+srv://hyesoo:<password>@boilerplate.a5yew.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+app.listen(port,()=>console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -72,6 +72,21 @@ userSchema.methods.generateToken = function(cb) {
     })
 }
 
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+    // 토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function(err, decoded){
+        //유저 아이디를 이용해서 유저를 찾은 다음에
+        //클라이언트에서 가져온 토큰과 데이터베이스에 보관된 토큰이 일치하는지 확인
+        //findOne은 몽고디비의 문법
+        //User id는 decode된 id이고, token은 찾는 token인 유저를 찾아요
+        user.findOne({"_id": decoded, "token": token},function(err,user){
+            if(err) return cb(err);
+            cb(null,user)
+        })
+    })
+}
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = {User}
