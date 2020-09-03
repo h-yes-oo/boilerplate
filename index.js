37e5b5bc48dd2373059648db98071b5fe43084a0
h-yes-oo <hyesoo5115@naver.com>
2020-09-03 18:56:23 +0900

    create-react-app

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const port = 5000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
-const config = require('./config/key');
-const { auth } = require("./middleware/auth");
-const { User } = require("./models/User");
+const config = require('./server/config/key');
+const { auth } = require("./server/middleware/auth");
+const { User } = require("./server/models/User");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
