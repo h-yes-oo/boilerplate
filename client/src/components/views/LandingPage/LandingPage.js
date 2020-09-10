d3ccfe2f93badd38a49bc346d78654be89c86a78
h-yes-oo <hyesoo5115@naver.com>
2020-09-10 09:11:34 +0900

    logout button

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React, {useEffect } from 'react';
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
     // 랜딩페이지에 들어오자마자 아래를 실행해요
     useEffect(() => {
         // 서버에 get request를 보내요 이때, endpoint는 /api/hello
@@ -9,11 +9,26 @@ function LandingPage() {
         .then(response => console.log(response.data))
         //위의 코드는, 서버에서 돌아온 response를 콘솔에 찍어주는 것 !! index.js를 보면 됩니다
     }, [])
+
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+            .then(response => {
+                if(response.data.success) {
+                    props.history.push("/login")
+                } else{
+                    alert('로그아웃에 실패했습니다.')
+                }
+            })
+    } 
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
         }}>
             <h2>시작 페이지</h2>
+
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
         </div>
     )
 }
